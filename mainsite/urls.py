15747b83013cecc2e8d6586367886c9228dfa768
UbadeAkkad <ubade.akkad@gmail.com>
2022-12-08 22:18:48 +0300

    Update

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('notadmin/', admin.site.urls),   # :D
     path('', include("home.urls")),
 ]
