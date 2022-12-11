62cc31b78d8d3258cc03ed7c5bb03b96078f9892
UbadeAkkad <ubade.akkad@gmail.com>
2022-12-11 19:46:03 +0300

    Update

--- notes/views.py
+++ notes/views.py
@@ -11,6 +11,7 @@ class NoteList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['notes'] = context['notes'].filter(user=self.request.user)
+        context['notes'] = context['notes'].order_by('-created')
         return context
 
 class CreateNote(LoginRequiredMixin, CreateView):
