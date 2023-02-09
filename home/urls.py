298293cf0babb4afc9cb0c6eaa59caa36ac5e791
UbadeAkkad <ubade.akkad@gmail.com>
2023-02-09 12:08:39 +0300

    Added robots.txt

--- home/urls.py
+++ home/urls.py
@@ -2,20 +2,22 @@ from django.urls import path, include
 from .views import LoginPage, Register, Homepage, GuestLogin, AddMessage, Git_Pull
 from django.contrib.auth.views import LogoutView
 from django.shortcuts import redirect
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path("login/", LoginPage.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(next_page='/'), name="logout"),
-    path("register/", Register.as_view(), name="register"),
+    path("login", LoginPage.as_view(), name="login"),
+    path("logout", LogoutView.as_view(next_page='/'), name="logout"),
+    path("register", Register.as_view(), name="register"),
     path("", Homepage.as_view() , name="home"),
     path("accounts/profile/",lambda request: redirect('home', permanent=False)),
     path('todo/', include("todo.urls")),
     path('notes/', include("notes.urls")),
-    path("convert/", include("guest_user.urls")),
+    path("convert", include("guest_user.urls")),
     path("guestlogin", GuestLogin, name="guestlogin"),
     path("message", AddMessage.as_view(), name="leaveamessage"),
     path('api/', include("api.urls")),
     path('react', include("react_app.urls")),
     path("git_pull/", Git_Pull, name="gitpull"),
-    path("github", lambda request: redirect("https://github.com/UbadeAkkad", permanent=False), name="githubaccount")
+    path("github", lambda request: redirect("https://github.com/UbadeAkkad", permanent=False), name="githubaccount"),
+    path("robots.txt",TemplateView.as_view(template_name="home/robots.txt", content_type="text/plain")),
 ]
