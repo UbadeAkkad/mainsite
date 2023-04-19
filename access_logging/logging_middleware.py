a4671bb311b98c4a9505ba36617973a71b59b3b3
UbadeAkkad <ubade.akkad@gmail.com>
2023-04-19 22:25:10 +0300

    Update logging_middleware.py

--- access_logging/logging_middleware.py
+++ access_logging/logging_middleware.py
@@ -35,10 +35,10 @@ class AccessLogsMiddleware(object):
                 pass
         else:
             try:
-                ip_api = "http://api.ipstack.com/{ip}?access_key={key}".format(key=config("API_IP_TOKEN"),ip=access_logs_data["ip_address"])
+                ip_api = "https://api.ipbase.com/v2/info?apikey={key}&ip={ip}".format(key=config("API_IP_TOKEN"),ip=access_logs_data["ip_address"])
                 data = json.loads(requests.get(ip_api).content)
-                access_logs_data["location"] = data["country_name"] + " / " + data["city"]
-                #access_logs_data["isp"] = data["connection"]["isp"]
+                access_logs_data["location"] = data["data"]["location"]["country"]["name"] + " / " + data["data"]["location"]["region"]["name"]
+                access_logs_data["isp"] = data["data"]["connection"]["isp"]
             except:
                 pass
 
