8a24f27325ea5a0712be3969384ff3dfe91190a1
UbadeAkkad <ubade.akkad@gmail.com>
2023-04-27 19:32:12 +0300

    Update admin.py

--- storage/admin.py
+++ storage/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import UploadedFile
+import os
 
 class UploadedFileAdmin(admin.ModelAdmin):
     list_display = ('file_name', 'download_link', 'uploaded_at')
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            if os.path.isfile(obj.file.path):
+                os.remove(obj.file.path)
+        super().delete_queryset(request, queryset)
 
 admin.site.register(UploadedFile, UploadedFileAdmin)
