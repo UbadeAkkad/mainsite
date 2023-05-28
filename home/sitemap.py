216b07eb0c0c3421162e58d4b97e46a1c2ab3312
UbadeAkkad <ubade.akkad@gmail.com>
2023-05-28 18:26:57 +0300

    Sitemap updated

--- home/sitemap.py
+++ home/sitemap.py
@@ -5,10 +5,24 @@ class StaticViewSitemap(Sitemap):
     changefreq = "monthly"
     
     def items(self):
-        return ['home',"githubaccount","leaveamessage","swagger-ui",]
+        return ['home',
+                "todo",
+                "notes",
+                "quiz_list",
+                "swagger-ui",
+                "leaveamessage",
+                "githubaccount",
+                "linkedinaccount",]
     
     def location(self, item):
         return reverse(item)
     
     def priority(self, item):
-        return {'home': 1.0, 'githubaccount': 1.0, 'leaveamessage': 0.7, 'swagger-ui': 0.6}[item]
+        return {'home': 1.0,
+                'todo': 0.7,
+                'notes': 0.7,
+                "quiz_list": 0.7,
+                'swagger-ui': 1.0,
+                'leaveamessage': 0.7,
+                'githubaccount': 1.0,
+                'linkedinaccount': 1.0,}[item]
